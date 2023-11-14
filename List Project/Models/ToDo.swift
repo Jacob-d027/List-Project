cd038991e30980f2aa1dda89f862d66fae423358
Jacob Davis <jacobdofficial@gmail.com>
2023-11-14 08:57:26 -0700

    Set up a couple of models and view

---
+++ List Project/Models/ToDo.swift
@@ -0,0 +1,20 @@
+//
+//  ToDo.swift
+//  List Project
+//
+//  Created by Jacob Davis on 11/14/23.
+//
+
+import Foundation
+
+struct ToDo: Equatable {
+    let id = UUID()
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    static func ==(lhs: ToDo, rhs: ToDo) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
